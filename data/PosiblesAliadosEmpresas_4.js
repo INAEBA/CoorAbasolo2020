2de16402ee36bb776687d126d065b46266800717
inaeba <mrochag@inaeba.edu.mx>
2021-09-13 11:38:10 -0500

    primera edicion

---
+++ data/PosiblesAliadosEmpresas_4.js
@@ -0,0 +1,11 @@
+var json_PosiblesAliadosEmpresas_4 = {
+"type": "FeatureCollection",
+"name": "PosiblesAliadosEmpresas_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nombre Emp": "PRESIDENCIA MUNICIPAL", "Razon Soci": "MUNICIPIO DE HUANIMARO", "Actividad": "Administraci�n p�blica en general", "Personas O": "251 y m�s personas", "Domicilio": "CALLE SEPTIMO CONGRESO No. 0 Manz. 6 COLONIA HUANIMARO CENTRO CP. 36990 Huan�maro", "Nomb_CCD": "HUANIMARO", "Clave_CCD": "I-11-017-03" }, "geometry": { "type": "Point", "coordinates": [ -101.4970485, 20.36819542 ] } },
+{ "type": "Feature", "properties": { "Nombre Emp": "CENTRO DE SALUD CON SERVICIOS AMPLIADOS CUERAMARO", "Razon Soci": "INSTITUTO DE SALUD PUBLICA DEL ESTADO DE GUANAJUATO", "Actividad": "Hospitales generales del sector p�blico", "Personas O": "51 a 100 personas", "Domicilio": "CALLE VENUSTIANO CARRANZA No. 400 Manz. 1 COLONIA CENTRO CP. 36960 Cuer�maro", "Nomb_CCD": "FRANCISCO VENEGAS", "Clave_CCD": "I-11-019-02" }, "geometry": { "type": "Point", "coordinates": [ -101.6722273, 20.63325548 ] } },
+{ "type": "Feature", "properties": { "Nombre Emp": "DIF MUNICIPAL", "Razon Soci": "PRECIDENCIA MUNICIPAL", "Actividad": "Actividades administrativas de instituciones de bienestar social", "Personas O": "51 a 100 personas", "Domicilio": "CALLE JESUS ORTEGA No. 0 Manz. 40 COLONIA RINCONADAS DE LA LUZ CP. 36960 Cuer�maro", "Nomb_CCD": "FRANCISCO VENEGAS", "Clave_CCD": "I-11-019-02" }, "geometry": { "type": "Point", "coordinates": [ -101.6692652, 20.62948398 ] } },
+{ "type": "Feature", "properties": { "Nombre Emp": "PRESIDENCIA MUNICIPAL", "Razon Soci": "GOBIERNO", "Actividad": "Administraci�n p�blica en general", "Personas O": "101 a 250 personas", "Domicilio": "CALLE FRANCISCO VENEGAS No. 111 Manz. 5 COLONIA ZONA CENTRO CP. 36960 Cuer�maro", "Nomb_CCD": "FRANCISCO VENEGAS", "Clave_CCD": "I-11-019-02" }, "geometry": { "type": "Point", "coordinates": [ -101.6704095, 20.6265589 ] } }
+]
+}
